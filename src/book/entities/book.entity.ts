35655f9a0814548e74fac6b880e0a77ff0612042
罗智毅 <luozhiyi@yaowutech.com>
2023-07-18 23:59:58 +0800

    feat: book表增加更新时间

--- src/book/entities/book.entity.ts
+++ src/book/entities/book.entity.ts
@@ -3,6 +3,7 @@ import {
   Column,
   PrimaryGeneratedColumn,
   CreateDateColumn,
+  UpdateDateColumn,
 } from 'typeorm';
 
 @Entity()
@@ -10,12 +11,23 @@ export class Book {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ type: 'varchar', length: 255 })
+  @Column({ type: 'varchar', length: 255, comment: '书籍名称' })
   name: string;
 
-  @Column({ type: 'varchar' })
+  @Column({ type: 'varchar', comment: '书籍描述' })
   description: string;
 
-  @CreateDateColumn({ type: 'timestamp' })
+  @CreateDateColumn({
+    name: 'create_time',
+    type: 'timestamp',
+    comment: '创建时间',
+  })
   createTime: Date;
+
+  @UpdateDateColumn({
+    name: 'updated_time',
+    type: 'datetime',
+    comment: '更新时间',
+  })
+  updatedTime: string;
 }
